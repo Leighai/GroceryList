98b1358bca811347295f5a1bd16c94d452e32708
AdrianTejada <atejada@my.bcit.ca>
2022-01-13 12:38:47 -0800

    weird bug where it adds data automatically

--- pages/index.js
+++ pages/index.js
@@ -42,22 +42,36 @@ const Title = styled.h1`
 
 
 export default function Home() {
+  const data = []
+
+  function AddItem (name, quantity, category) {
+    
+    data.push({
+      name,
+      quantity,
+      category
+    })
+    
+    console.log(data)
+  }
+
   return (
     <AppCont>
       <MainColumn>
         <Title>Popular Items</Title>
         <Column>
           <Subcolumn>
-            <GroceryItem imgUrl="/egg.png" />
-            <GroceryItem imgUrl="/egg.png" />
-            <GroceryItem imgUrl="/egg.png" />
+            <GroceryItem imgUrl="/egg.png" itemText="Eggs" />
+            <GroceryItem imgUrl="/egg.png" itemText="Coffee" />
+            <GroceryItem imgUrl="/egg.png" itemText="Bananas" onClick={AddItem("Bananas",1,"Fruits")}/>
           </Subcolumn>
           <Subcolumn>
-            <GroceryItem imgUrl="/egg.png" />
-            <GroceryItem imgUrl="/egg.png" />
-            <GroceryItem imgUrl="/egg.png" />
+            <GroceryItem imgUrl="/egg.png" itemText="Milk" />
+            <GroceryItem imgUrl="/egg.png" itemText="Pasta" />
+            <GroceryItem imgUrl="/egg.png" itemText="Carrots" />
           </Subcolumn>
         </Column>
+        <button onClick={()=>console.log(data)}>show data</button>
       </MainColumn>
       <MainColumn>
         <Title>Grocery List</Title>
